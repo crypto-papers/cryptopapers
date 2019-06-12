05f64f842849e8276b94987deef9d35d68f1deef
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-12 19:03:52 -0400

    Add basic about text (as markdown)

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
 // @flow
 
 import React from 'react'
-import Layout from "../components/Layout";
+import Markdown from 'react-markdown'
 
+import Layout from '../components/Layout';
+
+const about = `
+## About Us
+CryptoPapers.info is a simple online archive that lists papers related to cryptocurrency. The archive includes cryptocurrency whitepapers, research papers, and technical documentation and is intended for educational purposes. This site does not provide investment advice nor does a paper's inclusion in the archive imply an endorsement of it's contents.
+\n
+The site's codebase, as well as the project itself are open-sourced and contributions are encouraged.
+`
 export default () => (
-  <Layout>
-    <p>This is the about page</p>
+  <Layout title={ 'About' }>
+    <Markdown source={ about } />
   </Layout>
 )
