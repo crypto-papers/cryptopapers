ba4c7f81c667117e7912504a81b8ea2885c28887
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-12 19:50:35 -0400

    Create styles directory for variables and common styles

---
+++ styles/colors.js
@@ -0,0 +1,11 @@
+// @flow
+
+export const colors = {
+  darkBlue: '#053782', // rgba(5,55,130,1)
+  lightBlue: '#1d88e0', // rgba(29,136,224,1)
+  white: '#FFFFFF', // rgba(255,255,255,1)
+}
+
+export const gradients = {
+  darkLightBlue: 'linear-gradient(27deg, rgba(5,55,130,1) 0%, rgba(29,136,224,1) 100%)', // darkBlue to lightBlue
+}
