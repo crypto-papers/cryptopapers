d231ac9949826bd42d8f5fbde58dfff2348770d9
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-15 00:26:46 -0400

    Add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+// @flow
+
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
