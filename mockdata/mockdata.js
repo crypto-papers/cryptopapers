db11f5d362a1d4eac17122a913ee3856ff08cfdd
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-16 12:35:02 -0400

    Create a file for sharing mock data

---
+++ mockdata/mockdata.js
@@ -0,0 +1,11 @@
+export const mockPost = {
+  added: new Date(),
+  author: 'Test Author',
+  image: { url: 'https://cryptopapers.info/assets/covers/verge.png' },
+  pubDate: {
+    day: 20,
+    month: 'January',
+    year: 2014,
+  },
+  title: 'Verge Black Paper',
+};
