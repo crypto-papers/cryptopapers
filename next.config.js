369d06cfd6fdb7eead83bdcaaf8baa11efa6b879
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-16 18:12:13 -0400

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
-const withSASS = require('@zeit/next-sass')
-
-module.exports = withSASS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]_[hash:base64:5]',
-  },
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/p/hello-nextjs': {page: '/post', query: { title: 'Hello Next.js' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
-      '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } }
-    }
-  }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
+const withSASS = require('@zeit/next-sass');
+
+module.exports = withBundleAnalyzer(
+  withSASS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]_[hash:base64:5]',
+    },
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
+        '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
+        '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
+        '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } },
+      };
+    },
+  })
+);
