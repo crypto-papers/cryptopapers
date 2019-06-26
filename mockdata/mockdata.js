32ca7a8a252edd8b1d19e47fd83d5c521ff03801
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-25 23:11:49 -0400

    Add more mockdata

--- mockdata/mockdata.js
+++ mockdata/mockdata.js
@@ -14,11 +14,37 @@ export const mockPost = {
   title: 'Verge Black Paper',
 };
 
+export const mockPost1 = {
+  id: 2,
+  added: new Date(),
+  author: 'Nicolas van Saberhagen',
+  description:
+    'CryptoNote is a privacy-centric protocol that serves as the basis of several cryptocurrencies, most notably Monero. The paper begins by outlining some of the drawbacks inherent in Bitcoin, namely the traceability of transactions, the proof-of-work function, a predetermined emission rate, hardcoded constants, and bulky scripts. It presents the CryptoNote protocol as a way to address many of these shortcomings.',
+  downloadUrl: 'https://cryptopapers.info/assets/pdf/cryptonote.pdf',
+  image: { url: 'https://cryptopapers.info/assets/covers/cryptonote.png' },
+  pubDate: {
+    day: 17,
+    month: 'October',
+    year: 2013,
+  },
+  title: 'CryptoNote v2.0',
+};
+
+export const mockPost2 = {
+  id: 3,
+  added: new Date(),
+  description:
+    'This paper introduces Stox, "an open source, Ethereum based platform for prediction markets." It starts with an overview of prediction markets and their challenges. It then goes on to describe the Stox token (a Bancor ERC20 smart token), ecosystem (a global decentralized network), platform (where users can create and participate in prediction events), and app. The authors also provide a competitive analysis, roadmap, details on the token issuance, a list of team members, and an enumeration of some risk and technical considerations.',
+  downloadUrl: 'https://cryptopapers.info/assets/pdf/stox.pdf',
+  image: { url: 'https://cryptopapers.info/assets/covers/stox.png' },
+  title: 'Stox Platform for Prediction Markets',
+};
+
 export const mockPostMissingData = {
-  id: 1,
+  id: 4,
   added: new Date(),
   downloadUrl: 'https://cryptopapers.info/assets/pdf/verge.pdf',
   title: 'Ghost Protocol',
 };
 
-export const mockResults = [mockPost, mockPostMissingData, mockPost, mockPostMissingData];
+export const mockResults = [mockPost, mockPostMissingData, mockPost1, mockPost2];
