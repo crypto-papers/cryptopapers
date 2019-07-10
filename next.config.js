ab674765b61540a5a6a2a4a23dac0138af5b345e
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-10 17:20:41 -0400

    Prepare page for static exports

--- next.config.js
+++ next.config.js
@@ -14,10 +14,8 @@ module.exports = withBundleAnalyzer(
       return {
         '/': { page: '/' },
         '/about': { page: '/about' },
-        '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
-        '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
-        '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
-        '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } },
+        '/contribute': { page: '/contribute' },
+        '/resources': { page: '/resources' },
       };
     },
   })
--- pages/_app.js
+++ pages/_app.js
@@ -5,22 +5,7 @@ import App, { Container } from 'next/app';
 
 import 'styles/global.scss';
 
-type Props = {
-  Component: any,
-  ctx: any,
-};
-
 class CryptoPapers extends App {
-  static async getInitialProps({ Component, ctx }: Props) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
