fab8dd817e9366ad00dc913be80e2bd7c883ee4d
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-10 19:03:37 -0400

    Add directory for shared flow types

---
+++ types/customTypes.js
@@ -0,0 +1,32 @@
+// @flow
+
+/** @module customTypes */
+
+/**
+ * Data required to populated the featured paper
+ * @typedef PaperData
+ * @type {Object}
+ * @property {Date} added - JavaScript date object for date on which paper uploaded
+ * @property {string=} author - Name of paper's author
+ * @property {string=} description - Brief paper description
+ * @property {Object=} image - Image data (with properties url and alt)
+ * @property {Object} pubDate - Publication date info (with properties day, month, and year)
+ * @property {string} title - The title of a paper
+ * @memberof customTypes
+ */
+export type PaperData = {
+  added: Date,
+  author?: string,
+  description?: string,
+  downloadUrl: string,
+  image?: {
+    url?: string,
+    alt?: string,
+  },
+  pubDate: {
+    day?: number,
+    month?: string,
+    year?: number,
+  },
+  title: string,
+};
