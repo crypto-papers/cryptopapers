918a91948fbc83fafce357c443ca01aff0dd1b5f
Marek Rewers <mrewers@users.noreply.github.com>
2019-08-02 00:33:47 -0400

    Create component to list donation addresses

---
+++ components/Addresses/Addresses.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+
+import './Addresses.scss';
+
+/**
+ * JSX component that generates a list of cryptocurrency addresses
+ * @namespace Addresses
+ * @param {Array<string>} props - List of pages to add to a navigation menu.
+ */
+const Addresses = ({ currencies }: { currencies: Array<string> }) => {
+  if (!currencies || !Array.isArray(currencies)) return null;
+
+  return (
+    <ul styleName='address-list'>
+      {currencies.includes('eth') && <li>ETH: {process.env.ETH_ADDRESS}</li>}
+      {currencies.includes('btc') && <li>BTC: {process.env.BTC_ADDRESS}</li>}
+    </ul>
+  );
+};
+
+export default Addresses;
