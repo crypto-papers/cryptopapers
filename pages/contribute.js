f313351713b2de2e8436565344ce34d2874518b3
Marek Rewers <mrewers@users.noreply.github.com>
2019-08-02 00:34:49 -0400

    Move contribute page content into stand alone markdown file

--- pages/contribute.js
+++ pages/contribute.js
@@ -1,19 +1,13 @@
 // @flow
 
 import React from 'react';
-import Markdown from 'react-markdown';
 
 import Layout from '_components/Layout/Layout';
-
-const contribute = `
-CryptoPapers.info is a simple online archive that lists papers related to cryptocurrency. The archive includes cryptocurrency whitepapers, research papers, and technical documentation and is intended for educational purposes. This site does not provide investment advice nor does a paper's inclusion in the archive imply an endorsement of it's contents.
-\n
-The site's codebase, as well as the project itself are open-sourced and contributions are encouraged.
-`;
+import ContributeMdx from '_static/mdx/contribute.mdx';
 
 const Contribute = () => (
-  <Layout background={'geometric'} title={'Contribute'}>
-    <Markdown source={contribute} />
+  <Layout background={'geometric'} title={'We Need Your Help'}>
+    <ContributeMdx />
   </Layout>
 );
 
