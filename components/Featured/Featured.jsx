40964e51e3e93b742ca75d722fc86ba8aa891adf
Marek Rewers <mrewers@users.noreply.github.com>
2019-09-14 15:09:19 -0400

    Add file query to featured component

--- components/Featured/Featured.jsx
+++ components/Featured/Featured.jsx
@@ -6,7 +6,8 @@ import { QueryRenderer } from 'react-relay';
 
 import environment from '_schema/environment';
 import BlockQuote from '_components/BlockQuote/BlockQuote';
-import { concatDate, isoToLocale } from '_utils/conversions';
+import { isoToLocale } from '_utils/conversions';
+import { fileQuery } from '_lib/queries/file';
 import { paperQuery } from '_lib/queries/paper';
 import type { PaperData } from '_types/customTypes';
 
@@ -49,41 +50,57 @@ const Featured = ({ paperId }: { paperId: string }) => {
         /* eslint-disable react/prop-types */
         if (props && props.paper) {
           const { paper }: { paper: PaperData } = props;
-
-          const authorName = paper.author ? paper.author : 'Unknown';
-          const coverImage =
-            paper.image && paper.image.url ? paper.image.url : '/static/placeholder.svg';
-          const coverAlt =
-            paper.image && paper.image.alt ? paper.image.alt : `cover of ${paper.title}`;
-
           return (
-            <div styleName='featured-container'>
-              <figure styleName='featured-figure'>
-                <img styleName='featured-img' src={coverImage} alt={coverAlt} />
-              </figure>
-              <div styleName='featured-data'>
-                <h3 styleName='featured-title'>{paper.title}</h3>
-                {paper.excerpt && <BlockQuote quote={paper.excerpt} />}
-                <div styleName='featured-meta'>
-                  <p>
-                    <strong>Author: </strong>
-                    {authorName}
-                  </p>
-                  {paper.description && (
-                    <p>
-                      <strong>Description: </strong>
-                      {paper.description}
-                    </p>
-                  )}
-                  {paper.pubDate && (
-                    <p>
-                      <strong>Published on:</strong> {concatDate(paper.pubDate)}
-                    </p>
-                  )}
-                  <small styleName='featured-added'>Added on: {isoToLocale(paper.added)}</small>
-                </div>
-              </div>
-            </div>
+            <QueryRenderer
+              environment={environment}
+              query={fileQuery}
+              variables={{ id: paper.latestVersion }}
+              render={({ error, props }) => {
+                const authorName = paper.author ? paper.author : 'Unknown';
+
+                const hasFile = !error && props && props.file;
+                const file = hasFile ? props.file : {};
+
+                const coverImage = file.coverImage ? file.coverImage : '/static/placeholder.svg';
+                const coverAlt = `cover of ${paper.title}`;
+
+                return (
+                  <a styleName='featured-link' href={`/paper/${paper.prettyId}`}>
+                    <div styleName='featured-container'>
+                      <figure styleName='featured-figure'>
+                        <img styleName='featured-img' src={coverImage} alt={coverAlt} />
+                      </figure>
+                      <div styleName='featured-data'>
+                        <div styleName='featured-header'>
+                          <h3 styleName='featured-title'>{paper.title}</h3>
+                        </div>
+                        {paper.excerpt && <BlockQuote quote={paper.excerpt} />}
+                        <div styleName='featured-meta'>
+                          <p>
+                            <strong>Author: </strong>
+                            {authorName}
+                          </p>
+                          {paper.description && (
+                            <p>
+                              <strong>Description: </strong>
+                              {paper.description}
+                            </p>
+                          )}
+                          {paper.pubDate && (
+                            <p>
+                              <strong>Published on:</strong> {isoToLocale(paper.pubDate)}
+                            </p>
+                          )}
+                          <small styleName='featured-added'>
+                            Added on: {isoToLocale(paper.createAt)}
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                );
+              }}
+            />
           );
         }
         /* eslint-enable react/prop-types */
