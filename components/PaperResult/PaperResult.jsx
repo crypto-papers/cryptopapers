5d951c1ee6946587784a71fc422423eee958b1c7
Marek Rewers <mrewers@users.noreply.github.com>
2019-09-14 15:05:11 -0400

    Add file query to paper result component

--- components/PaperResult/PaperResult.jsx
+++ components/PaperResult/PaperResult.jsx
@@ -1,9 +1,12 @@
 // @flow
 
 import React, { Fragment } from 'react';
+import { QueryRenderer } from 'react-relay';
 
+import environment from '_schema/environment';
 import Loader from '_components/Loader/Loader';
-import { concatDate, dateToLocale } from '_utils/conversions';
+import { isoToLocale } from '_utils/conversions';
+import { fileQuery } from '_lib/queries/file';
 import type { PaperData } from '_types/customTypes';
 
 import './PaperResult.scss';
@@ -11,53 +14,64 @@ import './PaperResult.scss';
 /**
  * JSX component that renders the data for a specified paper.
  * @namespace PaperResult
- * @param {PaperData} data - A paper data object
+ * @param {PaperData} paper - A paper data object
  */
-const PaperResult = ({ data }: { data: PaperData }) => {
-  if (!data) {
+const PaperResult = ({ paper }: { paper: PaperData }) => {
+  if (!paper) {
     return <Loader />;
   }
 
-  console.log(data);
-
-  // const author = data.author ? `by ${data.author}` : '';
-  // const date = data.pubDate ? concatDate(data.pubDate) : '';
-
-  // let byline;
-  // if (author && date) {
-  //   byline = `${author} | ${date}`;
-  // } else if (author && !date) {
-  //   byline = author;
-  // } else if (!author && date) {
-  //   byline = date;
-  // }
-
   return (
-    <div>
-      <div styleName='paper-result-header'>
-        <h2>{data.title}</h2>
-        {/* <small styleName='paper-result-added'>Added: {dateToLocale(data.added)}</small> */}
-      </div>
-      {/* {byline && <p styleName='paper-result-byline'>{byline}</p>} */}
-      {data.description && (
-        <Fragment>
-          <strong>Description:</strong>
-          <p>{data.description}</p>
-        </Fragment>
-      )}
-      <object
-        data={data.downloadUrl}
-        height='1200'
-        width='100%'
-        styleName='paper-result-viewer'
-        type='application/pdf'
-      >
-        <p>
-          <strong>DOCUMENT UNAVAILABLE:</strong> We are unable to load PDFs in this browser. Please{' '}
-          <a href={data.downloadUrl}>download the PDF</a> to view it.
-        </p>
-      </object>
-    </div>
+    <QueryRenderer
+      environment={environment}
+      query={fileQuery}
+      variables={{ id: paper.latestVersion }}
+      render={({ error, props }) => {
+        const hasFile = !error && props && props.file;
+        const file = hasFile ? props.file : {};
+
+        const author = paper.author ? `by ${paper.author}` : '';
+        const pubDate = file.pubDate ? isoToLocale(file.pubDate) : null;
+        const url = file.url ? file.url : '';
+
+        let byline;
+        if (author && pubDate) {
+          byline = `${author} | ${pubDate}`;
+        } else if (author && !pubDate) {
+          byline = author;
+        } else if (!author && pubDate) {
+          byline = pubDate;
+        }
+
+        return (
+          <div>
+            <div styleName='paper-result-header'>
+              <h2>{paper.title}</h2>
+              <small styleName='paper-result-added'>Added: {isoToLocale(paper.createAt)}</small>
+            </div>
+            {byline && <p styleName='paper-result-byline'>{byline}</p>}
+            {paper.description && (
+              <Fragment>
+                <strong>Description:</strong>
+                <p>{paper.description}</p>
+              </Fragment>
+            )}
+            <object
+              paper={url}
+              height='1200'
+              width='100%'
+              styleName='paper-result-viewer'
+              type='application/pdf'
+            >
+              <p>
+                <strong>DOCUMENT UNAVAILABLE:</strong> We are unable to load PDFs in this browser.
+                Please <a href={url}>download the PDF</a> to view it.
+              </p>
+            </object>
+          </div>
+        );
+      }}
+    />
   );
 };
 
