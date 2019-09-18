fb9033b984e92845ccc50382f6082525d210b4da
Marek Rewers <mrewers@users.noreply.github.com>
2019-09-18 00:12:04 -0400

    Add query to fetch featured paper

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,9 @@ import Featured from '_components/Featured/Featured';
 import Layout from '_components/Layout/Layout';
 
 import environment from '_schema/environment';
+import { featureLatestQuery } from '_lib/queries/featureLatest';
 import { papersQuery } from '_lib/queries/papers';
-import { type ResultProp } from '_components/Result/Result';
+import type { ResultProp } from '_components/Result/Result';
 
 const ErrorMessage = dynamic(() =>
   import(
@@ -26,8 +27,34 @@ const ResultsGrid = dynamic(() =>
 
 const Homepage = () => (
   <Layout>
-    <h2>Featured Paper:</h2>
-    <Featured paperId='435c76bd-aae6-48ba-a259-ed4115e10a05' promoted />
+    <QueryRenderer
+      environment={environment}
+      query={featureLatestQuery}
+      render={({ error, props }) => {
+        if (props && props.featureLatest) {
+          return (
+            <Fragment>
+              <h2>Featured Paper:</h2>
+              <Featured
+                paperId={props.featureLatest.paper}
+                promoted={props.featureLatest.promoted}
+              />
+            </Fragment>
+          );
+        }
+
+        if (error) {
+          return (
+            <ErrorMessage
+              customMessage={'Apologies, we seem to be having trouble with that request'}
+              error={error}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
     <QueryRenderer
       environment={environment}
       // variables={{ count: 6 }}
