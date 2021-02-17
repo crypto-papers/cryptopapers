c556a2da061e29938e3a9f00b5c7b25a2f1917a7
Marek Rewers <hello@marek.dev>
2021-02-16 22:24:42 -0500

    Test: Use CryptoPapers ESLint and Prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    jest: true,
-    node: true,
-  },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
+    '@cryptopapers/eslint-config',
+    '@cryptopapers/eslint-config/react',
     'plugin:flowtype/recommended',
-    'prettier',
-    'prettier/babel',
-    'prettier/react',
     'prettier/flowtype',
   ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: [
-    'eslint-plugin-babel',
-    'eslint-plugin-react',
-    'eslint-plugin-jsx-a11y',
-    'eslint-plugin-flowtype',
-  ],
-  rules: {
-    'jsx-a11y/anchor-is-valid': ['warn'],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'react/prop-types': [0],
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect',
-    },
-  },
 };
