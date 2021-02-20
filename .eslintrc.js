af4762bac25002f6ec6f2973c7316b75278f0d8c
Marek Rewers <hello@marek.dev>
2021-02-19 20:04:53 -0500

    Build: Set up TypeScript linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   extends: ['@cryptopapers/eslint-config', '@cryptopapers/eslint-config/react'],
+  overrides: [
+    {
+      extends: [
+        '@cryptopapers/eslint-config',
+        '@cryptopapers/eslint-config/react',
+        '@cryptopapers/eslint-config/typescript',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+    },
+  ],
   parser: '@babel/eslint-parser',
 };
