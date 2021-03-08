1431f8a5ef729a42eec9a84efdc94a64dfac6fc0
Marek Rewers <hello@marek.dev>
2021-03-07 23:49:27 -0500

    Feat(admin): Send uploaded file to the api

--- admin/components/Uploader/Uploader.tsx
+++ admin/components/Uploader/Uploader.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import type { ChangeEvent, FC, ReactElement } from 'react';
 
+const sendUpload = async ( file: File ): Promise<any> => {
+  const data = new FormData();
+
+  data.append('file', file);
+  
+  const response = await fetch('http://localhost:4000/upload', {
+    body: data,
+    method: 'POST',
+    mode: 'no-cors',
+  })
+
+  const res = response.json();
+
+  return await res;
+}
+
 const handleUpload = (e: ChangeEvent<HTMLInputElement>): void => {
   const { files } = e.target;
   const file = files?.[0];
 
   if (file) {
-    const data = new FormData;
-
-    data.append('file', file);
+    sendUpload( file )
+      .then( () => console.log('Success'))
+      .catch(err => console.error(err));
   }
-
-  console.log(file);
 }
 
 const Uploader: FC = (): ReactElement => (
