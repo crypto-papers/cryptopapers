87f37f819123361f0e0933b0b6c23c9b78847a44
Marek Rewers <hello@marek.dev>
2021-10-08 18:53:37 -0400

    Feat(ui): Add uploader component from admin repo

---
+++ ui/src/components/Uploader/Uploader.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import type { ChangeEvent, FC, ReactElement } from 'react';
+
+const sendUpload = async (file: File): Promise<any> => {
+  const data = new FormData();
+
+  data.append('file', file);
+
+  const response = await fetch('http://localhost:4000/upload', {
+    body: data,
+    method: 'POST',
+  });
+
+  const res = response.json();
+
+  return await res;
+};
+
+const handleUpload = (e: ChangeEvent<HTMLInputElement>): void => {
+  const { files } = e.target;
+  const file = files?.[0];
+
+  if (file) {
+    sendUpload(file)
+      .then(() => console.log('Success'))
+      .catch(err => console.error(err));
+  }
+};
+
+const Uploader: FC = (): ReactElement => (
+  <form>
+    <input type="file" onChange={(e: ChangeEvent<HTMLInputElement>): void => handleUpload(e)} />
+  </form>
+);
+
+Uploader.displayName = 'Uploader';
+
+export default Uploader;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as BlockQuote } from './components/BlockQuote/BlockQuote';
 export { default as Header } from './components/Header/Header';
-export { default as Loader} from './components/Loader/Loader';
+export { default as Loader } from './components/Loader/Loader';
+export { default as Uploader } from './components/Uploader/Uploader';
