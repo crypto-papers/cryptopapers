87f6ee24054815c5b2b7f0ac2088b17284e56a43
Marek Rewers <hello@marek.dev>
2021-10-11 12:12:37 -0400

    Build(ui): Extract all CSS into one file and adjust scoped name template

--- ui/tsdx.config.js
+++ ui/tsdx.config.js
@@ -4,8 +4,11 @@ module.exports = {
   rollup(config) {
     config.plugins.push(
       postcss({
+        extract: 'cp-ui.css',
         minimize: true,
-        modules: true,
+        modules: {
+          generateScopedName: 'cpui_[local]_[hash:base64:8]',
+        },
       })
     );
 
