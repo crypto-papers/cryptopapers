7d1f9676f4d8f6b457bdfb1d3448e266d706c140
Marek Rewers <hello@marek.dev>
2021-10-15 01:04:15 -0400

    Feat(ui): Use actual blockquote HTML tag for the blockquote component

--- ui/src/components/BlockQuote/BlockQuote.stories.tsx
+++ ui/src/components/BlockQuote/BlockQuote.stories.tsx
@@ -4,17 +4,22 @@ import type { Meta } from '@storybook/react';
 import BlockQuote from './BlockQuote';
 
 const meta: Meta = {
-  component: BlockQuote,
-  title: 'Block Quote',
+  component: BlockQuote as React.FC,
+  title: 'Components/Block Quote',
 };
 
-const mockQuote = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.'
+const mockQuote =
+  'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.';
 
-const Quote: React.VFC = () => <BlockQuote quote={mockQuote} />;
+const Quote: React.VFC = () => (
+  <BlockQuote
+    cite="https://www.lipsum.com/"
+    quote={mockQuote}
+    source="Lorem Ipsum"
+    speaker="Richard McClintock"
+  />
+);
 
-export {
-  Quote
-};
+export { Quote };
 
 export default meta;
-
--- ui/src/components/BlockQuote/BlockQuote.tsx
+++ ui/src/components/BlockQuote/BlockQuote.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 import style from './BlockQuote.module.scss';
 
 interface BlockQuoteProps {
+  readonly cite?: string;
   readonly quote: string;
+  readonly source?: string;
+  readonly speaker?: string;
 }
 
 /**
  * JSX component that renders a bouncing ellipse loader item
+ * @param cite - A URL to point the user to the source of the quotation.
  * @param quote - Text to display in blockquote format.
  */
-const BlockQuote: React.FC<BlockQuoteProps> = ({ quote }) => (
-  <div>
-    <p className={style['blockquote-text']}>{quote}</p>
-  </div>
+const BlockQuote: React.FC<BlockQuoteProps> = ({ cite, quote, source = '', speaker = '' }) => (
+  <blockquote cite={cite} className={style.blockquote}>
+    <p className={style.text}>{quote}</p>
+    {(source || speaker) && (
+      <figcaption className={style.caption}>
+        {speaker ? <span>{speaker}</span> : ''}
+        {speaker && source ? ', ' : ''}
+        {source ? <cite>{source}</cite> : ''}
+      </figcaption>
+    )}
+  </blockquote>
 );
 
 BlockQuote.displayName = 'BlockQuote';
