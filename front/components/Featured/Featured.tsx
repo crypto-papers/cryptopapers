4194d3e09c308a89e279c6fe652f2d2ece6f6d27
Marek Rewers <hello@marek.dev>
2021-10-19 23:24:33 -0400

    Feat(front): Replace promoted badge component with that from UI library

--- front/components/Featured/Featured.tsx
+++ front/components/Featured/Featured.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import { QueryRenderer } from 'react-relay';
-import { BlockQuote, Loader } from '@cryptopapers/ui';
-
+import { BlockQuote, Loader, Promoted } from '@cryptopapers/ui';
 import environment from 'schema/environment';
 import { isoToLocale } from 'utils/conversions';
 import { fileQuery } from 'lib/queries/file';
@@ -11,17 +10,12 @@ import type { FileData, PaperData } from 'types/customTypes';
 
 import style from './Featured.module.scss';
 
-const ErrorMessage = dynamic(() =>
-  import(
-    /* webpackChunkName: "errorMessage" */
-    'components/ErrorMessage/ErrorMessage'
-  )
-);
-const Promoted = dynamic(() =>
-  import(
-    /* webpackChunkName: "promoted" */
-    'components/Promoted/Promoted'
-  )
+const ErrorMessage = dynamic(
+  async () =>
+    await import(
+      /* WebpackChunkName: "errorMessage" */
+      'components/ErrorMessage/ErrorMessage'
+    )
 );
 
 /**
@@ -30,85 +24,84 @@ const Promoted = dynamic(() =>
  * @param {string} paperId - A paper id value
  * @param {boolean} promoted - Is the paper a promoted post
  */
-const Featured = ({ paperId, promoted }: { paperId: string, promoted?: boolean }) => {
-  return (
-    <QueryRenderer
-      environment={environment}
-      query={paperQuery}
-      variables={{ id: paperId }}
-      render={({ error, props }) => {
-        if (error) {
-          return (
-            <ErrorMessage
-              customMessage={'Apologies, we seem to be having trouble with that request'}
-              error={error}
-            />
-          );
-        }
+const Featured = ({ paperId, promoted }: { paperId: string; promoted?: boolean }) => (
+  <QueryRenderer
+    environment={environment}
+    query={paperQuery}
+    render={({ error, props }) => {
+      if (error) {
+        return (
+          <ErrorMessage
+            customMessage="Apologies, we seem to be having trouble with that request"
+            error={error}
+          />
+        );
+      }
 
-        if (props && props.paper) {
-          const { paper }: { paper: PaperData } = props;
-          return (
-            <QueryRenderer
-              environment={environment}
-              query={fileQuery}
-              variables={{ id: paper.latestVersion }}
-              render={({ error, props }) => {
-                if (!error && props) {
-                  const file: FileData = props.file || {};
+      if (props && props.paper) {
+        const { paper }: { paper: PaperData } = props;
 
-                  const authorName = paper.author ? paper.author : 'Unknown';
-                  const coverImage = file.coverImage ? file.coverImage : '/static/placeholder.svg';
-                  const coverAlt = `cover of ${paper.title}`;
-                  const id = paper.prettyId ? paper.prettyId : '';
-                  const title = paper.subTitle ? `${paper.title}: ${paper.subTitle}` : paper.title;
+        return (
+          <QueryRenderer
+            environment={environment}
+            query={fileQuery}
+            render={({ error, props }) => {
+              if (!error && props) {
+                const file: FileData = props.file || {};
 
-                  return (
-                    <a className={style['featured-link']} href={`/paper/${id}`}>
-                      <div className={style['featured-container']}>
-                        <figure className={style['featured-figure']}>
-                          <img className={style['featured-img']} src={coverImage} alt={coverAlt} />
-                        </figure>
-                        <div className={style['featured-data']}>
-                          <div className={style['featured-header']}>
-                            <h3 className={style['featured-title']}>{title}</h3>
-                            {promoted && <Promoted />}
-                          </div>
-                          {paper.excerpt && <BlockQuote quote={paper.excerpt} />}
-                          <div className={style['featured-meta']}>
+                const authorName = paper.author ? paper.author : 'Unknown';
+                const coverImage = file.coverImage ? file.coverImage : '/static/placeholder.svg';
+                const coverAlt = `cover of ${paper.title}`;
+                const id = paper.prettyId ? paper.prettyId : '';
+                const title = paper.subTitle ? `${paper.title}: ${paper.subTitle}` : paper.title;
+
+                return (
+                  <a className={style['featured-link']} href={`/paper/${id}`}>
+                    <div className={style['featured-container']}>
+                      <figure className={style['featured-figure']}>
+                        <img alt={coverAlt} className={style['featured-img']} src={coverImage} />
+                      </figure>
+                      <div className={style['featured-data']}>
+                        <div className={style['featured-header']}>
+                          <h3 className={style['featured-title']}>{title}</h3>
+                          {promoted && <Promoted />}
+                        </div>
+                        {paper.excerpt && <BlockQuote quote={paper.excerpt} />}
+                        <div className={style['featured-meta']}>
+                          <p>
+                            <strong>Author: </strong>
+                            {authorName}
+                          </p>
+                          {paper.description && (
                             <p>
-                              <strong>Author: </strong>
-                              {authorName}
+                              <strong>Description: </strong>
+                              {paper.description}
                             </p>
-                            {paper.description && (
-                              <p>
-                                <strong>Description: </strong>
-                                {paper.description}
-                              </p>
-                            )}
-                            {file.pubDate && (
-                              <p>
-                                <strong>Published on:</strong> {isoToLocale(file.pubDate)}
-                              </p>
-                            )}
-                            <small className={style['featured-added']}>
-                              Added on: {isoToLocale(paper.createAt)}
-                            </small>
-                          </div>
+                          )}
+                          {file.pubDate && (
+                            <p>
+                              <strong>Published on:</strong> {isoToLocale(file.pubDate)}
+                            </p>
+                          )}
+                          <small className={style['featured-added']}>
+                            Added on: {isoToLocale(paper.createAt)}
+                          </small>
                         </div>
                       </div>
-                    </a>
-                  );
-                }
-              }}
-            />
-          );
-        }
+                    </div>
+                  </a>
+                );
+              }
+            }}
+            variables={{ id: paper.latestVersion }}
+          />
+        );
+      }
 
-        return <Loader />;
-      }}
-    />
-  );
-};
+      return <Loader />;
+    }}
+    variables={{ id: paperId }}
+  />
+);
 
 export default Featured;
--- front/components/Promoted/Promoted.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import style from './Promoted.module.scss';
-
-const Promoted = () => <small className={style['promoted-tag']}>Promoted</small>;
-
-export default Promoted;
