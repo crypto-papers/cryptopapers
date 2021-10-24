63a2736b9fa633cb643345a2d8d4e74305dea638
Marek Rewers <hello@marek.dev>
2021-10-23 22:42:37 -0400

    Feat(ui): Adjust header padding depending on presence of top nav

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import Plume from '../../static/plume.svg';
 import style from './Header.module.scss';
 
 interface IHeaderProps {
+  readonly nav?: boolean;
   readonly skip?: string;
   readonly title: string;
 }
@@ -13,11 +14,12 @@ interface IHeaderProps {
  * A JSX component that renders the page header.
  * @component
  * @param props
+ * @param props.nav - Whether room should be reserved at the top of the header for the nav bar.
  * @param props.skip - The element id to be used as the destination for the skip link.
  * @param props.title - The text used to populate the header title.
  */
-const Header: React.FC<IHeaderProps> = ({ skip, title }) => (
-  <div className={style.header}>
+const Header: React.FC<IHeaderProps> = ({ nav = false, skip, title }) => (
+  <div className={nav ? `${style.header} ${style['with-nav']}` : style.header}>
     {Boolean(skip) && (
       <a className={style.skip} href={skip}>
         Skip to main content
